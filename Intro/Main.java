e336f6327731cb85b0e55320d8ed108a36af478d
Cameron L <imcelarson@gmail.com>
2025-09-19 11:16:00 -0600

    Created new directories for the files I've already worked on

---
+++ Intro/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		//This is a comment
+		/* Multi
+		line
+		comment*/
+		System.out.println("Hello World!");
+		System.out.println("I am learning Java.");
+		System.out.print("Hello World! ");
+		System.out.print("I will print on the same line.");
+		System.out.println(3);
+		System.out.println(3+3);
+		System.out.println(3*5);
+		System.out.println("10"+5);
+	}
+}
---
+++ Variables/Variables.java
@@ -0,0 +1,56 @@
+public class Variables {
+	public static void main(String[] args) {
+		// String name = "John";
+		// System.out.println(name);
+		// int myNum = 15;
+		// System.out.println(myNum);
+		
+		// int myNum;
+		// myNum = 30;
+		// System.out.println(myNum);
+		
+		// int newNum = 10;
+		// newNum = newNum * 2;
+		// System.out.println(newNum);
+		
+		// final keyword declares a variable as unchangable	
+		final int aNum = 35;
+		// aNum = 25; // This will raise an error
+		
+		// Printing Variables
+		String name = "John";
+		System.out.println("Hello " + name);
+		
+
+		String firstName = "John";
+		String lastName = "Doe";
+		String fullName = firstName + " " + lastName;
+		System.out.println(fullName);
+		
+		// Can perform arithmetic on difference int vars
+		// int x = 5;
+		// int y = 6;
+		// System.out.println(x + y);
+		
+		// int x = 5;
+		// int y = 6;
+		// int z = 50;
+		// System.out.println(x + y + z);
+		
+		// int x = 5, y = 6, z = 50;
+		// System.out.println(x + y + z);
+				
+		// int x, y, z;
+		// x = y = z = 50;
+		// System.out.println(x + y + z);
+		
+		// Identifiers make variables more clear
+		// int minutesPerHour = 60;
+		
+		// final int MINUTES_PER_HOUR = 60;
+		// final int BIRTHYEAR = 1980;
+		
+		// System.out.println(BIRTHYEAR);
+		
+	}
+}
