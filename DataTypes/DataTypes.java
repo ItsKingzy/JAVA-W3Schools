8b261fc077b9f489012b42a7becd8eacd640322c
Cameron L <imcelarson@gmail.com>
2025-09-19 11:45:48 -0600

    Finished Numbers lesson

--- DataTypes/DataTypes.java
+++ DataTypes/DataTypes.java
@@ -18,7 +18,37 @@ public class DataTypes {
 		*/
 		
 		
+		// Numbers
 		
+		// Byte is 8-bits, (-127 to 128)
+		byte myNum0 = 100;
+		System.out.println(myNum0);
+		
+		// Short ranged from -32768 to 32767
+		short myNum1 = 5000;
+		System.out.println(myNum1);
+		
+		// Int holds from -2147483648 to 2147483647 
+		// int myNum = 100000;
+		// System.out.println(myNum);
+		
+		// Long hold from -9223372036854775808 to 9223372036854775807
+		// long myNum = 15000000000L;
+		// System.out.println(myNum);
+		
+		// Floats
+		// float myNum = 5.75f;
+		// System.out.println(myNum);
+		
+		// double myNum = 19.99d;
+		// System.out.println(myNum);
+		
+		float f1 = 35e3f;
+		double d1 = 12E4d;
+		System.out.println(f1);
+		System.out.println(d1);
+
+
 	}
 
 
