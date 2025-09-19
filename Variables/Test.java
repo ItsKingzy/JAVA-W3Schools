7706c37685c22ea958db8ade43360ba2084f0a5b
Cameron L <imcelarson@gmail.com>
2025-09-19 11:26:32 -0600

    Finished the final exercise for Variables in JAVA

---
+++ Variables/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+	public static void main(String[] args) {
+		// Student Data
+		String studentName = "John Doe";
+		int studentID = 15;
+		int studentAge = 23;
+		float studentFee = 75.25f;
+		char studentGrade = 'B';
+		
+		// Print Variables
+		System.out.println("Student name: " + studentName);
+		System.out.println("Student id: " + studentID);
+		System.out.println("Student age: " + studentAge);
+		System.out.println("Student fee: " + studentFee);
+		System.out.println("Student grade: " + studentGrade);
+	}
+}
