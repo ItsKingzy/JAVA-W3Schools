312c0513bd828917bdcbb331cb6949710ae3e457
Cameron L <imcelarson@gmail.com>
2025-09-19 11:36:11 -0600

    Finished Data types lesson 1 moving to lesson 2 (numbers)

---
+++ DataTypes/DataTypes.java
@@ -0,0 +1,26 @@
+public class DataTypes {
+	public static void main(String[] args) {
+		int myNum = 5;
+		float myFloatNum = 5.99f;
+		char myLetter = 'D';
+		boolean myBool = true;
+		String myText = "Hello";
+		
+		/*
+		Primitive types: byte, short, int, long, float, double,
+		boolean, char
+		- Usually short values and immutable
+		*/
+		
+		/*
+		Non-privitive types: String, Arrays, Classes
+		- Complex data types that hold lots of data, usually mutable
+		*/
+		
+		
+		
+	}
+
+
+
+}
